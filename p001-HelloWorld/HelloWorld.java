a22ac44a6c97b365c35885a408d97569cf29e119
Drausin Wulsin <daedalus2718@gmail.com>
2013-04-21 23:11:29 -0400

    initial commit of p001-HelloWorld project

---
+++ p001-HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	/**
+	 * Our first Java program!
+	 * 
+	 * @author Drausin Wulsin
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
